7e5a05399e7b5b34a35d0addccc86f72dd878999
microze33 <124433131+microze33@users.noreply.github.com>
2023-02-04 04:03:53 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+
+inloc= (float(input("What is the initial location?")))
+intvelo= (float(input("What is ther initial velocity?")))
+accel= (float(input("What is the acceleration?")))
+time= (float(input("What is the time?")))
+finalloc= (inloc+intvelo+(1/2(accel*time**2)))
+print(finalloc)
